eda15f8a7baae5b6c7cc468af0cb1fbce595e02e
Orlando Brown <orlando.brown@stepstone.com>
2021-01-19 15:56:45 +0000

    chore: remove index files

--- frontend/src/components/index.js
+++
@@ -1,5 +0,0 @@
-export { Title } from './Title';
-export { Details } from './Details';
-export { Description } from './Description';
-export { CompanyLogo } from './CompanyLogo';
-export { Button } from './Button';
